0fce9c763e0cdd639f39eacf6a9fcb005ce9798d
LlamosasNG <gonzalezllamosas07@gmail.com>
2024-09-26 21:00:04 -0600

    Práctica 1 Modificación de la formula de regresión lineal para el ejercicio A Agregué archivo del ejercicio B

---
+++ P01_Exercise01.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 1.- Definir los datos de entrada 
+X = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+Yd = np.array([0.8, 2.95, 2.3, 3.6, 5.2, 5.3, 6.1, 5.9, 7.6, 9])
+
+# 2.- Calcular los coeficientes de la regresión lineal
+m = len(Yd) 
+a = ((m * np.sum(X * Yd)) - (np.sum(X) * np.sum(Yd))) / ((m * np.sum(X ** 2)) - (np.sum(X) ** 2))
+b = (np.sum(Yd) - a * np.sum(X)) / m
+print(f"a = {a}")
+print(f"b = {b}")
+
+# 3.- Calcular los valores estimados (Yobt)
+Yobt = a * X + b
+
+ECM = (1 / (2 * m)) * np.sum(Yobt - Yd) **2
+print(ECM)
+
+# 4.- Grafica de las funciones
+plt.scatter(X, Yd, color='blue', label='Datos originales')  # Graficar puntos 
+plt.plot(X, Yobt, color='red', label='Recta de regresión')  # Graficar línea
+
+plt.xlabel('X')  
+plt.ylabel('Yd')  
+plt.title('Regresión Lineal Simple')  
+
+plt.show()
