a57c18804e18f2f194a1db0a6bd03d35f584d916
LlamosasNG <gonzalezllamosas07@gmail.com>
2024-10-01 03:50:11 -0600

    Ejercicio de Función Sigmoidal Se implementa un ejercicio con la función sigmoidal

---
+++ Exercise03.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+# Definir la función sigmoide
+def Sigmoide(z):
+    return 1/(1 + np.exp(-z))
+    # z = theta * X
+
+# Datos de entrenamiento
+X = np.array([[1,2], [1,3], [1,4], [1,5]])
+Yd = np.array([0, 0, 1, 1])
+Yobt = np.zeros(len(Yd))
+J = 0
+
+# Definiendo hiperparámetros 
+theta = np.array([.5, 1])
+
+# Hiperpárametrosdentro del modelo
+lr = 0.01
+epocas = 200
+m = np.array(Yd)
+
+# for i in range(epocas):
+# Realizar el producto punto de cada una de mis muestras con el vector theta
+Z = np.dot(X, theta)
+H = Sigmoide(Z)
+
+for i in range(len(H)):
+    if (H[i] >= 0.5):
+        Yobt[i] = 1
+    else:
+        Yobt[i] = 0
+    
+    # Evaluar la función de costo
+    J = -(1/m) * np.sum(Yd * np.log(H) + (1-Yd) * np.log(1-H))
+    print(J)
+
+    # theta = theta - lr * (1/m) * np.dot((Yd - H), X)
+
+    # print(np.dot((Yd - H), X))
+
+    
+    
