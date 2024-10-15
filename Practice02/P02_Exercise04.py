5c80f0aec0885dbd87a7b8d95137850d3cb5405f
LlamosasNG <gonzalezllamosas07@gmail.com>
2024-10-14 18:33:46 -0600

    Cambios en la P02

--- Practice02/P02_Exercise04.py
+++ Practice02/P02_Exercise04.py
@@ -17,13 +17,17 @@ def ValidateH(H):
     return Predictions
 
 # Datos de entrenamiento
-X = np.array([[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[1,8],[1,9],[1,10],[1,11],[1,12],[1,13],[1,14],[1,15],[1,16],[1,17],[1,18],[1,19],[1,20],[1,21]])
-Yd = np.array([0,0,1,1,0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,0])
-Yobt = np.zeros(len(Yd))
+quantity = int(input("Cantidad de datos que quieres ingresar en X e Yd \n"))
+X = np.zeros(quantity)
+Yd = np.zeros(quantity)
+
+for i in range(quantity):
+    X[i] = float(input(f"Valor de X[{i}]: "))
+    Yd[i] = float(input(f"Valor de Yd[{i}]: "))
 
 # Definiendo hiperparámetros
 n = len(X[0])
-theta = np.zeros(n)  
+theta = np.zeros(n)
 theta0 = 0
 
 # Hiperparámetros dentro del modelo
@@ -31,18 +35,17 @@ lr = 0.1
 epocas = 1000
 m = len(Yd)
 
-# Array para almacenar los valores de J
 J_hist = np.zeros(epocas)
 
 for i in range(epocas):
-    Z = theta0 + np.dot(X, theta)
+    Z = np.dot(X, theta) 
     H = Sigmoide(Z)
     
     # Evaluar la función de costo
     J = -(1/m) * np.sum(Yd * np.log(H) + (1 - Yd) * np.log(1 - H))
     
     J_hist[i] = J
-
+    
     theta0 = theta0 - lr * (1/m) * np.sum(H - Yd)
     theta = theta - lr * (1/m) * np.dot(X.T, (H - Yd))
     
@@ -54,6 +57,7 @@ Yobt = ValidateH(H)
 print(f"Theta0: {theta0}")
 print(f"Theta: {theta}")
 
+# Graficar los resultados
 plt.scatter(X[:, 1], Yd, color='blue', label='Datos de entrenamiento')
 
 J_hist_scaled = (J_hist - np.min(J_hist)) / (np.max(J_hist) - np.min(J_hist))
